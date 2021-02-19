8f48a5d7641836c8bb9158d8ba7745fac57fea1c
Tom MARTINS <Tom@Toms-MacBook-Pro.local>
2021-02-19 09:51:25 +0100

    Adding distributed caching for tokens

--- src/main/java/lu/raiffeisen/gateway/apigateway/ApiGatewayApplication.java
+++ src/main/java/lu/raiffeisen/gateway/apigateway/ApiGatewayApplication.java
@@ -2,8 +2,10 @@ package lu.raiffeisen.gateway.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
--- src/main/java/lu/raiffeisen/gateway/apigateway/filters/BearerTokenExchangeGatewayFilterFactory.java
+++ src/main/java/lu/raiffeisen/gateway/apigateway/filters/BearerTokenExchangeGatewayFilterFactory.java
@@ -1,10 +1,18 @@
 package lu.raiffeisen.gateway.apigateway.filters;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.Security;
 import java.util.Arrays;
 import java.util.List;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.util.encoders.Hex;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.json.JsonParserFactory;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpHeaders;
@@ -32,27 +40,41 @@ public class BearerTokenExchangeGatewayFilterFactory
     public static String ISSUER_CONTAINER_TYPE_NONE = "none";
     public static String ISSUER_CONTAINER_TYPE_BY_VALUE = "value";
     public static String ISSUER_CONTAINER_TYPE_HEADER = "header";
+    public static String CACHE_MANAGER_CACHE = "token-echange";
     
-	@Value("${bearer-token-exchange.auth-server-url}")
+	@Value("${bearer-token-exchange.auth-server-url:#{null}}")
     private String authServerBaseUrl;
 	
-	@Value("${bearer-token-exchange.endpoint-uri}")
+	@Value("${bearer-token-exchange.endpoint-uri:#{null}}")
     private String tokenEndpointUrl;
 	
-	@Value("${bearer-token-exchange.client-id}")
+	@Value("${bearer-token-exchange.client-id:#{null}}")
     private String clientId;
 	
-	@Value("${bearer-token-exchange.client-secret}")
+	@Value("${bearer-token-exchange.client-secret:#{null}}")
     private String clientSecret;
 	
-	@Value("${bearer-token-exchange.issuer-container-type}")
+	@Value("${bearer-token-exchange.issuer-container-type:#{null}}")
     private String issuerContainerType;
 	
-	@Value("${bearer-token-exchange.issuer-container-named}")
-    private String issuerContainerName;    
+	@Value("${bearer-token-exchange.issuer-container-named:#{null}}")
+    private String issuerContainerName;
+	
+	@Value("${bearer-token-exchange.audience:#{null}}")
+    private String audience;
+	
+	@Value("${bearer-token-exchange.scope:#{null}}")
+    private String scope;
+	
+	private CacheManager cacheManager;
+	private Cache tokensCache;
 
-    public BearerTokenExchangeGatewayFilterFactory() {
+    public BearerTokenExchangeGatewayFilterFactory(CacheManager cacheManager) {
         super(Config.class);
+        this.cacheManager = cacheManager;
+        this.tokensCache = this.cacheManager.getCache(BearerTokenExchangeGatewayFilterFactory.CACHE_MANAGER_CACHE);
+        
+        Security.addProvider(new BouncyCastleProvider());
     }
 
     private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus)  {
@@ -76,9 +98,24 @@ public class BearerTokenExchangeGatewayFilterFactory
     private String getSubjectIssuerByHeader(ServerWebExchange exchange, String headerName) {
         return exchange.getRequest().getHeaders().getFirst(headerName);
     }
+    
+    private String tokenHash(String token) {
+        MessageDigest digest;
+        String sha256hex = null;
+		try {
+			digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(token.getBytes(StandardCharsets.UTF_8));
+            sha256hex = new String(Hex.encode(hash));
+		} catch (NoSuchAlgorithmException e) {
+			log.error(e.getMessage());
+			e.printStackTrace();
+		}
+		return sha256hex;
+    }
 
     @Override
     public GatewayFilter apply(Config config) {
+    	
         return (exchange, chain) -> {
 
             ServerHttpRequest request = exchange.getRequest();
@@ -88,42 +125,59 @@ public class BearerTokenExchangeGatewayFilterFactory
             if (!token.contains("Bearer"))
                 return this.onError(exchange, "No bearer authorization token", HttpStatus.UNAUTHORIZED);
             String bearerToken = token.replace("Bearer", "").trim();
+            
+            String bearerTokenHash = this.tokenHash(bearerToken);
+            if (bearerTokenHash == null || bearerTokenHash.isEmpty())
+            	return this.onError(exchange, "Issue hashing token", HttpStatus.INTERNAL_SERVER_ERROR);
+            
+            String cachedToken = tokensCache.get(bearerTokenHash, String.class);
+            if (cachedToken != null && !cachedToken.isEmpty()) {
+            	log.debug("Exchange token gotten from cache..");
+                exchange.getRequest().mutate().header(HttpHeaders.AUTHORIZATION, "Bearer " + cachedToken);
+                return chain.filter(exchange);
+            } else {
 
-            MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
-            formData.add("client-id", (config.getClientId() != null ? config.getClientId() : this.clientId));
-            formData.add("client-secret", (config.getClientSecret() != null ? config.getClientSecret() : this.clientSecret));
-            formData.add("grant_type", BearerTokenExchangeGatewayFilterFactory.GRANT_TYPE);
-            formData.add("requested_token_type", BearerTokenExchangeGatewayFilterFactory.REQUESTED_TOKEN_TYPE);
-            formData.add("subject_token", bearerToken);
-            String subjectIssuer = getSubjectIssuer(exchange, config);
-            if (subjectIssuer != null)
-            	formData.add("subject_issuer", subjectIssuer);
-            if (config.getAudience() != null)
-            	formData.add("audience", config.getAudience());
-            if (config.getScope() != null)
-            	formData.add("scope", config.getScope());
+            	MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+            	formData.add("client_id", (config.getClientId() != null ? config.getClientId() : this.clientId));
+            	formData.add("client_secret", (config.getClientSecret() != null ? config.getClientSecret() : this.clientSecret));
+            	formData.add("grant_type", BearerTokenExchangeGatewayFilterFactory.GRANT_TYPE);
+            	formData.add("requested_token_type", BearerTokenExchangeGatewayFilterFactory.REQUESTED_TOKEN_TYPE);
+            	formData.add("subject_token", bearerToken);
+            	String subjectIssuer = getSubjectIssuer(exchange, config);
+            	if (subjectIssuer != null)
+            		formData.add("subject_issuer", subjectIssuer);
+            	String audienceValue = (config.getAudience() != null ? config.getAudience() : this.audience);
+            	if (audienceValue != null)
+            		formData.add("audience", audienceValue);
+            	String scopeValue = (config.getScope() != null ? config.getScope() : this.scope);
+            	if (scopeValue != null)
+            		formData.add("scope", scopeValue);
 
-            return WebClient.create().post()
-                .uri((config.getAuthServerBaseUrl() != null ? config.getAuthServerBaseUrl() : this.authServerBaseUrl) + (config.getTokenEndpointUrl() != null ? config.getTokenEndpointUrl() : this.tokenEndpointUrl))
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .body(BodyInserters.fromFormData(formData))
-                .exchange()
-                .flatMap(response -> {
-                    if (!response.statusCode().equals(HttpStatus.OK))
-                        return this.onError(exchange, "exchange request failed", HttpStatus.BAD_REQUEST);
+            	return WebClient.create().post()
+            			.uri((config.getAuthServerBaseUrl() != null ? config.getAuthServerBaseUrl() : this.authServerBaseUrl) + (config.getTokenEndpointUrl() != null ? config.getTokenEndpointUrl() : this.tokenEndpointUrl))
+            			.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            			.body(BodyInserters.fromFormData(formData))
+            			.exchange()
+            			.flatMap(response -> {
+            				if (!response.statusCode().equals(HttpStatus.OK))
+            					return this.onError(exchange, "exchange request failed", HttpStatus.BAD_REQUEST);
 
-                    return response.bodyToMono(String.class)
-                        .flatMap(body -> {
-                            String accessToken = (String) JsonParserFactory.getJsonParser().parseMap(body).get("access_token");
+            				return response.bodyToMono(String.class)
+            						.flatMap(body -> {
+            							String accessToken = (String) JsonParserFactory.getJsonParser().parseMap(body).get("access_token");
 
-                            if (accessToken == null || accessToken.trim().isEmpty())
-                                return this.onError(exchange, "no token provided", HttpStatus.BAD_REQUEST);
+            							if (accessToken == null || accessToken.trim().isEmpty())
+            								return this.onError(exchange, "no token provided", HttpStatus.BAD_REQUEST);
 
-                            exchange.getRequest().mutate().header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken);
+            							this.tokensCache.put(bearerTokenHash, accessToken);
+            							log.debug("Exchanged token has been cached..");
+                            
+            							exchange.getRequest().mutate().header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken);
 
-                            return chain.filter(exchange);
-                        });
-                });
+            							return chain.filter(exchange);
+            						});
+            			});
+            }
         };
     }
 
